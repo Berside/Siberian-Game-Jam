9a247d0ec8da644ae8cf91ceefeca5ac10050ec1
MonkaS <danchik646@gmail.com>
2024-11-02 20:34:16 +0300

    стрельба

--- Assets/Player/WeaponAim.cs
+++ Assets/Player/WeaponAim.cs
@@ -6,26 +6,35 @@ using UnityEngine.EventSystems;
 
 public class WeaponAim : MonoBehaviour
 {
-    public event EventHandler<OnShootEventArgs> onShoot;
-    public class OnShootEventArgs: EventArgs
-    {
-        public Vector3 gunEndPointPosition;
-        public Vector3 shootPosition;
-    }
-
     private Transform aimTransform;
+
+    [SerializeField]
+    protected GameObject bulletPrefab;
+
+    [SerializeField]
+    protected float bulletSpeed;
+
+    private Transform gunEndPointPosition;
+    private float time = 0;
+
+    private WeaponStats weaponStats;
 
     // Start is called before the first frame update
     void Awake()
     {
         aimTransform = transform.Find("Aim");
+        gunEndPointPosition = aimTransform.GetChild(0).Find("GunEndPointPosition").transform;
+        weaponStats = aimTransform.GetChild(0).GetComponent<WeaponStats>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        time += Time.deltaTime;
+
         HandleAiming();
         HandleShooting();
+        HandleReload();
     }
 
     private void HandleAiming()
@@ -39,10 +48,39 @@ public class WeaponAim : MonoBehaviour
 
     private void HandleShooting()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (weaponStats.FullAuto())
+        { 
+            if (Input.GetButton("Fire1") && weaponStats.getCurrentAmmo() > 0)
+            {
+                if (time >= weaponStats.ShootCooldown())
+                {
+                    // here happens shooting :)
+                    GameObject bullet = Instantiate(bulletPrefab, gunEndPointPosition.position, gunEndPointPosition.rotation);
+                    Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+                    weaponStats.fire();
+                    rb.AddForce(gunEndPointPosition.right * bulletSpeed, ForceMode2D.Impulse);
+                    time = 0;
+                }
+            }
+        }
+        else
         {
-            // here happens shooting :)
+            if (Input.GetButtonDown("Fire1") && weaponStats.getCurrentAmmo() > 0)
+            {
+                // here happens shooting :)
+                GameObject bullet = Instantiate(bulletPrefab, gunEndPointPosition.position, gunEndPointPosition.rotation);
+                Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+                weaponStats.fire();
+                rb.AddForce(gunEndPointPosition.right * bulletSpeed, ForceMode2D.Impulse);
+            }
+        }
+    }
 
+    private void HandleReload()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            weaponStats.reload();
         }
     }
 
