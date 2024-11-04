02fbd251a25899d8c6a9231c998a09e8079e6719
MonkaS <danchik646@gmail.com>
2024-11-04 16:09:22 +0300

    добавил имена в прологе, сделал перемотку пролога в обе стороны (ПКМ, ЛКМ)

--- Assets/prologue/DialogueManager.cs
+++ Assets/prologue/DialogueManager.cs
@@ -10,24 +10,26 @@ public class DialogueManager : MonoBehaviour
 {
     public Text textArea;
 
+    public Text charName;
+
     public Image Cop;
     public Image Bro;
 
     private Tuple<string, string>[] messages = {
-        new Tuple<string, string>("Cop", "Ты снова упариваешься этой наркотой!?"),
-        new Tuple<string, string>("Bro", "Это не твое дело"),
-        new Tuple<string, string>("Bro", "*занюхивает*"),
-        new Tuple<string, string>("Bro", "Что со мной!?"),
-        new Tuple<string, string>("Bro", "АААААААААААААААА"),
-        new Tuple<string, string>("Bro", "*Корчится от боли*"),
-        new Tuple<string, string>("Cop", "Что с тобой? Что ты вынюхал?"),
-        new Tuple<string, string>("Bro", "Барыга п*****с подсунул метадон"),
-        new Tuple<string, string>("Bro", "Меня уже не спасти(((((("),
-        new Tuple<string, string>("Cop", "Онееееееет не умирай!!!"),
-        new Tuple<string, string>("Bro", "……"),
-        new Tuple<string, string>("Bro", ""),
-        new Tuple<string, string>("Cop", "Пора покончить с этим раз и навсегда!"),
-        new Tuple<string, string>("Cop", "*Звуки шагов в сторону нарко притона*"),
+        new Tuple<string, string>("Рик", "Ты снова упариваешься этой наркотой!?"),
+        new Tuple<string, string>("Сауль", "Это не твое дело"),
+        new Tuple<string, string>("Сауль", "*занюхивает*"),
+        new Tuple<string, string>("Сауль", "Что со мной!?"),
+        new Tuple<string, string>("Сауль", "АААААААААААААААА"),
+        new Tuple<string, string>("Сауль", "*Корчится от боли*"),
+        new Tuple<string, string>("Рик", "Что с тобой? Что ты вынюхал?"),
+        new Tuple<string, string>("Сауль", "Барыга п*****с подсунул метадон"),
+        new Tuple<string, string>("Сауль", "Меня уже не спасти(((((("),
+        new Tuple<string, string>("Рик", "Онееееееет не умирай!!!"),
+        new Tuple<string, string>("Сауль", "……"),
+        new Tuple<string, string>("Сауль", ""),
+        new Tuple<string, string>("Рик", "Пора покончить с этим раз и навсегда!"),
+        new Tuple<string, string>("Рик", "*Звуки шагов в сторону нарко притона*"),
     };
 
     private int message = 0;
@@ -48,11 +50,12 @@ public class DialogueManager : MonoBehaviour
 
         var currentMessage = messages[message];
 
-        if (currentMessage.Item1 == "Cop")
+        if (currentMessage.Item1 == "Рик")
         {
             Bro.enabled = false;
             Cop.enabled = true;
-            textArea.transform.localPosition = new Vector3(283, transform.position.y, transform.position.z);
+            charName.transform.localPosition = new Vector3(-396, charName.transform.localPosition.y, charName.transform.localPosition.z);
+            textArea.transform.localPosition = new Vector3(283, textArea.transform.localPosition.y, textArea.transform.localPosition.z);
         }
         else
         {
@@ -62,14 +65,23 @@ public class DialogueManager : MonoBehaviour
             }
             Bro.enabled = true;
             Cop.enabled = false;
-            textArea.transform.localPosition = new Vector3(-214, transform.position.y, transform.position.z);
+            charName.transform.localPosition = new Vector3(493, charName.transform.localPosition.y, charName.transform.localPosition.z);
+            textArea.transform.localPosition = new Vector3(-214, textArea.transform.localPosition.y, textArea.transform.localPosition.z);
         }
 
+        charName.text = currentMessage.Item1;
         textArea.text = currentMessage.Item2;
 
         if (Input.GetButtonDown("Fire1"))
         {
             message++;
+        }
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if (message > 0)
+                message--;
+            else
+                message = 0;
         }
 
         if (Input.GetKeyDown(KeyCode.Escape))
